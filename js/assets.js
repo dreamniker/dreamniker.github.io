013d97f521bfcd175cd3d2dab15ae508a9ee8b0a
dreamniker <dreamniker@gmail.com>
2014-09-02 21:37:09 +0400

    Revert "hi"
    This reverts commit 55be0a0aadba22854af193576d8aaca6c95e98ae.

--- js/assets.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-function Assets (){
-	this.person = new Image ();
-	this.person.src = "img/person.png";	
-	
-	this.mazeSprite = new Image ();
-	this.mazeSprite.src = "img/TilesSprite.png";
-	
-}
